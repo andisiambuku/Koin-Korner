a969db62787dd9c131e4232a90a47112867c83ff
andisiambuku <roselandambuku@gmail.com>
2022-09-09 11:48:39 +0300

    market data table debug and styling

--- src/script.js
+++ src/script.js
@@ -64,54 +64,36 @@ function marketData(){
     return fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     
     .then(res=>res.json())
-    .then(data=>display(data));
+    .then(data=>displayTable(data));
     
 }
-function display(data){   
-     for(let i=0;i<data.length;i++){
-        const oneCoin = data[i];
-        console.log(oneCoin);
-        const index = i +1;
-        const logo = oneCoin.image;
-        const name = oneCoin.name;
-        const symbol = oneCoin.symbol;
-        const market_cap= oneCoin.market_cap;
-        const current_price=oneCoin.current_price.toFixed(2);
-        const price_change = oneCoin.price_change_percentage_24h.toFixed(2)
-        
-        createCoinCard(index, logo, name, symbol, market_cap,current_price,price_change);
-        
-    }
-          
-}
 
-  
+function displayTable(data){
+    const $resultsTable = document.getElementById('market-data-table')
 
-function createCoinCard(index, logo, name, symbol, market_cap,current_price,price_change){
-    const id_element = document.createElement('p');
-    if(index<10){
-        index = index + "&nbsp;&nbsp;";
-    }
-    id_element.innerHTML=index;
+    data.forEach((element,index) =>{
+        renderRow(element,index)
+    })
 
-    const logo_element = document.createElement('img');
-    logo_element.src=logo;
-    logo_element.alt='Coin Logo';
+    function renderRow(element,position){
+       const {image, name, symbol, market_cap, current_price, price_change_percentage_24h} =element
 
-    const name_element = document.createElement('h3');
-    name_element.innerText= name;
+    // first row is taken up by the headers
+    // insert a new row based on the index to the table element
+    const row = $resultsTable.insertRow(position + 1)
+    // each cell inserted corresponds to a row 
+    const numberCell = row.insertCell(0)
+    const imageCell = row.insertCell(1)
+    const nameCell = row.insertCell(2)
+    const symbolCell = row.insertCell(3)
+    const currentPriceCell =  row.insertCell(4)
+    const marketCapPriceCell = row.insertCell(5)
+    const priceChangeCell = row.insertCell(6)
+    const likerCell = row.insertCell(7)
 
-    const symbol_element = document.createElement('h3');
-    symbol_element.innerText=symbol;
-
-    const current_price_element = document.createElement('h3');
-    current_price_element.innerText= "$" + current_price;
-   
-    const market_cap_element = document.createElement('h3');
-    market_cap_element.innerText= "$" + market_cap;
-
-    const price_change_element = document.createElement('h3');
-    price_change_element.innerText=price_change + "%";
+    const logoElement = document.createElement('img')
+    logoElement.src = image;
+    logoElement.alt = 'Coin-Logo';
 
     const likerButton = document.createElement('button');
     likerButton.innerHTML="	&#11088;";
@@ -119,20 +101,18 @@ function createCoinCard(index, logo, name, symbol, market_cap,current_price,pric
         alert("Liked");
     }
 
-    const container_element = document.createElement('div');
-    container_element.classList.add('single-market-data','card');
-    container_element.appendChild(id_element);
-    container_element.appendChild(logo_element);
-    container_element.appendChild(name_element);
-    container_element.appendChild(symbol_element);
-    container_element.appendChild(current_price_element);
-    container_element.appendChild(market_cap_element);
-    container_element.appendChild(price_change_element);
-    container_element.appendChild(likerButton);
     
-    
-    document.getElementById('market-data').appendChild(container_element);
+    numberCell.innerText = position + 1
+    imageCell.appendChild(logoElement)
+    nameCell.innerText = name.toUpperCase()
+    symbolCell.innerText = symbol.toUpperCase()
+    currentPriceCell.innerText = `$${current_price.toFixed(2)}`
+    marketCapPriceCell.innerText = `$${market_cap.toFixed(2)}`
+    priceChangeCell.innerText = `${price_change_percentage_24h.toFixed(2)}%`
+    likerCell.appendChild(likerButton)
+    }
 }
+  
 
 window.onload = function(){
     searchData();
